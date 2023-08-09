fc4df4bf23cec42b954928f1cf60204034f5c133
AnyFaty-Mejia <141791294+AnyFaty-Mejia@users.noreply.github.com>
2023-08-09 15:12:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function ocultarMostrar() {
+    console.log("Acción Ocultar mostrar");
+  
+    var contenido = document.getElementById("contenidoOcultar");
+    console.log("Contenido valor tiene " + contenido.hidden);
+    if (contenido.hidden) {
+      contenido.hidden = false;
+    } else {
+      contenido.hidden = true;
+    }
+  }
+
+  function ocultarMostrar1() {
+    console.log("Acción Ocultar mostrar");
+  
+    var contenido = document.getElementById("contenidoOcultar1");
+    console.log("Contenido valor tiene " + contenido.hidden);
+    if (contenido.hidden) {
+      contenido.hidden = false;
+    } else {
+      contenido.hidden = true;
+    }
+  }
